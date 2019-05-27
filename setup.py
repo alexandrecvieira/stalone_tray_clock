d57f926758576f6c66a7537993508265a6272711
Alexandre C Vieira <acamargo.vieira@gmail.com>
2019-05-27 15:37:07 -0300

    Tentativa fracassada de portar para python3

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
 import glob
 import os
 from DistUtilsExtra.command import *
-
-
-
 
 setup(name='stalone_tray_clock',
       version='1.0.0',
